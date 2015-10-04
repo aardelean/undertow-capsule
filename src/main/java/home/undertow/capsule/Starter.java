2a7999fb1cff278dc758fbc9a0271d57eec15cfa
unknown <alexandru.ardelean@gmail.com>
2015-10-04 16:00:02 +0200

    added support for spring

--- src/main/java/home/undertow/capsule/Starter.java
+++ src/main/java/home/undertow/capsule/Starter.java
@@ -1,9 +1,12 @@
 package home.undertow.capsule;
 
+import co.paralleluniverse.springframework.boot.autoconfigure.web.FiberSpringBootApplication;
+import co.paralleluniverse.springframework.boot.autoconfigure.web.FiberWebMvcAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
@@ -15,10 +18,17 @@ import org.springframework.web.WebApplicationInitializer;
 /**
  * Created by alex on 9/5/2015.
  */
+@FiberSpringBootApplication
 @ComponentScan("home.undertow.capsule")
 @PropertySource("classpath:application.properties")
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-@Import({PersistenceJPAConfig.class, CacheConfiguration.class, RestConfiguration.class,MongoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
+                                    HibernateJpaAutoConfiguration.class,
+                                    WebMvcAutoConfiguration.class})
+@Import({PersistenceJPAConfig.class,
+            CacheConfiguration.class,
+            RestConfiguration.class,
+            FiberWebMvcAutoConfiguration.class,
+            MongoConfiguration.class})
 public class Starter extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     @Override
